59c41d308e11b09be89718d37c72f91afef88095
Jason Kuhrt <jasonkuhrt@me.com>
2015-01-05 16:22:54 -0500

    Use Express for HTTP server

--- seed-cloudbit-reader/index.js
+++ seed-cloudbit-reader/index.js
@@ -1,45 +1,46 @@
-var http = require('http')
+var express = require('express')
+var bodyParser = require('body-parser')
+var morgan = require('morgan')
+var app = express()
 
 
 
-var server = http.createServer(function(request, response) {
-  console.log('Handling new request: method %j | URI %j ', request.method, request.url)
+/* Middleware */
 
-  var bodyString = ''
+app.use(bodyParser.json())
+app.use(morgan('dev'))
 
-  request.on('data', function(bodyChunk) {
-    console.log('Received data chunk received: %j', String(bodyChunk))
-    bodyString += String(bodyChunk)
-  })
 
-  request.once('end', function() {
-    /* On any GET respond with a friendly message explaining that this
-    application has no interesting client-side component. */
-    if (request.method === 'GET') return response.end('Hello, this is a trivial cloudBit Reader App. Nothing else to see here; all the action happens server-side. Confused? On the CLI use `$ heroku logs` to see any recent input activity from webhook-registered cloudBits.')
 
-    /* On any POST respond with a 200 OK string. Yup, this is very liberal. */
-    console.log('Received POST body: %j', bodyString)
+/* Rotues */
 
-    /* Try to safely parse the BODY */
-    var cloudBitEvent
-    try {
-      cloudBitEvent = JSON.parse(bodyString)
-    } catch (jsonParseError) {
-      response.end('ERROR')
-      return console.error('There was a JSON parse error on the POST body: %j. The error was: %j', bodyString, jsonParseError)
-    }
-
-    handleCloudbitEvent(cloudBitEvent)
-    response.writeHead(200, {'Content-Type': 'text/plain'})
-    response.end('OK')
-  })
+/* On a root GET respond with a friendly message explaining that this
+application has no interesting client-side component. */
+app.get('/', function(req, res) {
+  res.send('Hello, this is a trivial cloudBit Reader App. Nothing else to see here; all the action happens server-side. Confused? On the CLI use `$ heroku logs` to see any recent input activity from webhook-registered cloudBits.')
 })
 
-server.listen(process.env.PORT || 3000, function() {
-  console.log('App booted at: %j', server.address())
+/* On a root POST log info about the (should be) cloudBit event. */
+app.post('/', function(req, res) {
+  handleCloudbitEvent(req.body)
+  res.send('OK')
 })
 
 
+
+/* Boot the server. */
+
+var server = app.listen(process.env.PORT || 3000, function() {
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('App booted at http://%s:%s', host, port)
+})
+
+
+
+
+// Helpers
 
 function handleCloudbitEvent(event) {
   switch (event.type) {
