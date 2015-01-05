3026ebbb1cab0f0a679bd0cf7e2eb850edb42659
Jason Kuhrt <jasonkuhrt@me.com>
2015-01-05 14:50:44 -0500

    Update logging to only access non-depricated parts of the event schema

--- seed-cloudbit-reader/index.js
+++ seed-cloudbit-reader/index.js
@@ -39,10 +39,7 @@ function handleCloudbitEvent(event) {
   switch (event.type) {
     case 'amplitude':
       // Do whatever you want with the amplitde
-      var percent = event.payload.percent
-      var level = event.payload.level
-      var delta = event.payload.delta
-      console.log('cloudBit amplitude: percent @ %d% | level @ %s | delta @ %s', percent, level, delta)
+      console.log('cloudBit input received: %d%', event.payload.percent)
       break
     case 'connectionChange':
       // One day, cloudBits will emit this event too, but not yet.
